2b15e06328715c67503c5af49c03fc33d9403d98
Amit Singh <amitsingh19975@gmail.com>
2021-03-02 16:18:24 +0530

    refactoring code

--- src/dot.cpp
+++ src/dot.cpp
@@ -23,11 +23,10 @@ namespace ub = boost::numeric::ublas;
 
 
 template<typename ValueType, std::size_t MaxIter = 100ul>
-void ublas_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
+void ublas_dot_prod(std::vector<double> const& x, amt::metric<ValueType>& m){
     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
 
-    std::string fn_name = std::string(__func__) 
-        + (std::is_same_v<ValueType,float> ? "_float" : "_double");
+    std::string_view fn_name = "Boost.ublas";
     
     auto& metric_data = m[fn_name];
     constexpr auto bench_fn = [](auto const& v1, auto const& v2){
@@ -44,11 +43,10 @@ void ublas_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
 }
 
 template<typename ValueType, std::size_t MaxIter = 100ul>
-void mkl_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
+void mkl_dot_prod(std::vector<double> const& x, amt::metric<ValueType>& m){
     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
     
-    std::string fn_name = std::string(__func__) 
-        + (std::is_same_v<ValueType,float> ? "_float" : "_double");
+    std::string_view fn_name = "intel MKL";
     
     auto& metric_data = m[fn_name];
     constexpr auto bench_fn = [](auto&&... args){
@@ -70,11 +68,10 @@ void mkl_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
 
 #ifdef AMT_BENCHMARK_OPENBLAS_HPP
 template<typename ValueType, std::size_t MaxIter = 100ul>
-void openblas_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
+void openblas_dot_prod(std::vector<double> const& x, amt::metric<ValueType>& m){
     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
     
-    std::string fn_name = std::string(__func__) 
-        + (std::is_same_v<ValueType,float> ? "_float" : "_double");
+    std::string_view fn_name = "OpenBlas";
     
     auto& metric_data = m[fn_name];
     constexpr auto bench_fn = [](auto&&... args){
@@ -92,11 +89,10 @@ void openblas_same_layout(std::vector<double> const& x, amt::metric<ValueType>& 
 #endif
 
 template<typename ValueType, std::size_t MaxIter = 100ul>
-void openmp_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
+void openmp_dot_prod(std::vector<double> const& x, amt::metric<ValueType>& m){
     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
     
-    std::string fn_name = std::string(__func__) 
-        + (std::is_same_v<ValueType,float> ? "_float" : "_double");
+    std::string_view fn_name = "experimental::boost(OpenMP)";
     
     auto& metric_data = m[fn_name];
     ValueType ret{};
@@ -114,47 +110,11 @@ void openmp_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m)
     }
 }
 
-// template<std::size_t Start, std::size_t End, typename ValueType>
-// int static_tensor_same_layout(amt::metric<ValueType>& m){
-//     using namespace boost::mp11;
-//     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
-    
-//     std::string fn_name = std::string(__func__) 
-//         + (std::is_same_v<ValueType,float> ? "_float" : "_double");
-    
-//     auto& metric_data = m[fn_name];
-//     ValueType ret{};
-
-//     using number_list = mp_iota_c<End>;
-//     using range = mp_drop_c<number_list, std::max(1ul, Start) >;
-
-//     mp_for_each<range>([&](auto I){
-//         constexpr auto sz = decltype(I)::value;
-//         double const ops = 2. * sz;
-//         using extents_type = ub::static_extents<1ul, sz>;
-//         using tensor_type = ub::static_tensor<ValueType,extents_type>;
-//         tensor_type v1, v2;
-//         double st{};
-//         auto k = max_iter;
-//         while(k--){
-//             amt::timer t{};
-//             {   
-//                 amt::dot_prod(ret, v1, v2, t);
-//             }
-//             st += t();
-//         }
-//         st /= static_cast<double>(max_iter);
-//         metric_data.update((ops / st));
-//     });
-//     return static_cast<int>(ret);
-// }
-
 template<typename ValueType, std::size_t MaxIter = 100ul>
-void blis_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
+void blis_dot_prod(std::vector<double> const& x, amt::metric<ValueType>& m){
     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
     
-    std::string fn_name = std::string(__func__) 
-        + (std::is_same_v<ValueType,float> ? "_float" : "_double");
+    std::string_view fn_name = "Blis";
     
     auto& metric_data = m[fn_name];
     ValueType ret{};
@@ -176,13 +136,13 @@ void blis_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
 }
 
 template<typename ValueType, std::size_t MaxIter = 100ul>
-void eigen_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
+void eigen_dot_prod(std::vector<double> const& x, amt::metric<ValueType>& m){
     using namespace Eigen;
     using vector_type = Matrix<ValueType,-1,1,ColMajor>;
     static_assert( std::is_same_v<ValueType,float> || std::is_same_v<ValueType,double>, "ValueType not supported" );
     
-    std::string fn_name = std::string(__func__) 
-        + (std::is_same_v<ValueType,float> ? "_float" : "_double");
+    std::string_view fn_name = "Eigen";
+    // Eigen::setNbThreads(16);
     
     auto& metric_data = m[fn_name];
 
@@ -197,44 +157,36 @@ void eigen_same_layout(std::vector<double> const& x, amt::metric<ValueType>& m){
         double st = amt::benchmark<MaxIter>(bench_fn, v1, v2);
         metric_data.update((ops / st));
     }
+
 }
 
-// #define DIFFERENT_LAYOUT
 // #define DISABLE_PLOT
 // #define SPEEDUP_PLOT
 #define PLOT_ALL
 
 int main(){
+    Eigen::initParallel();
     using value_type = float;
     // using value_type = double;
     std::vector<double> x;
     [[maybe_unused]]constexpr std::size_t max_iter = 100ul;
-    [[maybe_unused]]constexpr double max_value = 16382;
+    [[maybe_unused]]constexpr double max_value = 32 * 1024;
     amt::range(x, 32., max_value, 32., std::plus<>{});
     // [[maybe_unused]]constexpr double max_value = (1u<<20);
     // amt::range(x, 2., max_value, 1024., std::plus<>{});
     // amt::range(x, 2., max_value, 2., std::multiplies<>{});
 
     auto m = amt::metric<value_type>(x.size());
-    // exit(0);
-    #ifndef DIFFERENT_LAYOUT
-        ublas_same_layout<value_type,max_iter>(x,m);
-        openblas_same_layout<value_type,max_iter>(x,m);
-        blis_same_layout<value_type,max_iter>(x,m);
-        eigen_same_layout<value_type,max_iter>(x,m);
-        openmp_same_layout<value_type,max_iter>(x,m);
-        mkl_same_layout<value_type,max_iter>(x,m);
-        // std::cout<<m.tail();
-    #else
-        // res += ref_dot_diff_layout<value_type,ub::layout::first_order>(x,m);
-        // res += openblas_dot_diff_layout<value_type,ub::layout::first_order>(x,m);
-        // res += tensor_dot_diff_layout<value_type,ub::layout::first_order>(x,m);
-        // res += blis_dot_diff_layout<value_type,ub::layout::first_order>(x,m);
-        // res += eigen_dot_diff_layout<value_type,ub::layout::first_order>(x,m);
 
-    #endif
+    ublas_dot_prod<value_type,max_iter>(x,m);
+    openblas_dot_prod<value_type,max_iter>(x,m);
+    blis_dot_prod<value_type,max_iter>(x,m);
+    eigen_dot_prod<value_type,max_iter>(x,m);
+    openmp_dot_prod<value_type,max_iter>(x,m);
+    mkl_dot_prod<value_type,max_iter>(x,m);
+    // std::cout<<m.tail();
 
-    // std::cout<<m.str("openmp")<<'\n';
+    // std::cout<<m.str("OpenMP")<<'\n';
     #ifndef DISABLE_PLOT
         #if !defined(SPEEDUP_PLOT) || defined(PLOT_ALL)
             m.plot(x);
@@ -242,8 +194,8 @@ int main(){
         #endif
         
         #if defined(SPEEDUP_PLOT) || defined(PLOT_ALL)
-            // m.plot_speedup("openmp",x);
-            m.plot_speedup_per("openmp");
+            // m.plot_speedup("OpenMP",x);
+            m.plot_speedup_per("OpenMP");
         #endif
     #endif
     // m.raw();
