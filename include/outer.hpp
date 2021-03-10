34b262b60e9910c267590081e5ab7380fa802030
Amit Singh <amitsingh19975@gmail.com>
2021-03-10 17:31:58 +0530

    improve outer product

--- include/outer.hpp
+++ include/outer.hpp
@@ -31,8 +31,7 @@ namespace amt {
         [[maybe_unused]] static auto const number_of_el_l1 = cache_manager::size(0) / size_in_bytes;
         [[maybe_unused]] static auto const number_of_el_l2 = cache_manager::size(1) / size_in_bytes;
         [[maybe_unused]] static auto const number_of_el_l3 = cache_manager::size(2) / size_in_bytes;
-        [[maybe_unused]] static auto const block1 = number_of_el_l2 >> 1;
-        [[maybe_unused]] static auto const block2 = number_of_el_l1 >> 1;
+        [[maybe_unused]] static auto const block = number_of_el_l1 >> 1;
 
         constexpr auto simd_loop = [](Out* c, In const* a, In const* b, SizeType const n){
             auto cst = *b;
@@ -52,21 +51,12 @@ namespace amt {
 
         #pragma omp parallel if(nb > max_bl)
         {
-            for(auto i = 0ul; i < nb; i += block1){
-                auto aii = ai;
-                auto bii = bi + i;
-                auto cii = ci + i * wc;
-                auto ib = std::min(block1, nb - i);
-                #pragma omp for nowait 
-                for(auto ii = 0ul; ii < ib; ++ii){
-                    auto aj = aii;
-                    auto bj = bii + ii;
-                    auto cj = cii + ii * wc;
-                    for(auto j = 0ul; j < na; j += block2){
-                        auto jb = std::min(block2, na - j);
-                        simd_loop(cj + j, aj + j, bj, jb);
-                    }
-                }
+            #pragma omp for nowait 
+            for(auto i = 0ul; i < nb; ++i){
+                auto aj = ai;
+                auto bj = bi + i;
+                auto cj = ci + i * wc;
+                simd_loop(cj, aj, bj, na);
             }
         }
 
