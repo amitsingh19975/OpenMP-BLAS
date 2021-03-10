f952ac7a7bf4b3fc3a048f703c9a8b88aa88d8df
Amit Singh <amitsingh19975@gmail.com>
2021-03-10 14:17:13 +0530

    add test for different layout

--- test/test.outer_prod.cpp
+++ test/test.outer_prod.cpp
@@ -28,7 +28,7 @@ constexpr auto blis_ger(conj_t  conjx,
     }
 };
 
-TEMPLATE_TEST_CASE( "Square Vector Vector Outer Product for Range[Start: 2, End: 32, Step: 1]", "[outer_prod]", float, double ) {
+TEMPLATE_TEST_CASE( "First Order Square Vector Vector Outer Product for Range[Start: 2, End: 32, Step: 1]", "[first_order_outer_prod]", float, double ) {
     namespace ub = boost::numeric::ublas;
 
     constexpr std::size_t max_size = 1 << 5;
@@ -56,13 +56,50 @@ TEMPLATE_TEST_CASE( "Square Vector Vector Outer Product for Range[Start: 2, End:
         // amt::blas::outer_prod<TestType>(amt::blas::ColMajor, M, N, TestType{1}, a.data(), inc, a.data(), inc, rres.data(), M);
         amt::outer_prod(lres, a, a, std::nullopt);
 
-        for(auto i = 0ul; i < (sz * sz); ++i)
-            REQUIRE(Approx(lres[i]) == rres[i]);
+        for(auto i = 0ul; i < sz; ++i){
+            for(auto j = 0ul; j < sz; ++j)
+                REQUIRE(Approx(lres(i,j)) == rres(i,j));
+        }
     }
     
 }
 
-TEMPLATE_TEST_CASE( "Rectangular Vector Vector Outer Product for Range[Start: 2, End: 32, Step: 1]", "[outer_prod]", float, double ) {
+TEMPLATE_TEST_CASE( "Last Order Square Vector Vector Outer Product for Range[Start: 2, End: 32, Step: 1]", "[last_order_outer_prod]", float, double ) {
+    namespace ub = boost::numeric::ublas;
+
+    constexpr std::size_t max_size = 1 << 5;
+    std::vector<std::size_t> sizes(max_size);
+    std::iota(sizes.begin(), sizes.end(), 2u);
+    
+    REQUIRE(sizes.size() == max_size);
+
+
+    for(auto const& sz : sizes){
+        VectorType<TestType> a(ub::extents<2>{1,sz});
+        
+        ub::dynamic_tensor<TestType, ub::layout::last_order> lres(ub::extents<>{sz, sz});
+        ub::dynamic_tensor<TestType, ub::layout::last_order> rres(ub::extents<>{sz, sz});
+        std::iota(a.begin(), a.end(), 1);
+        
+        auto inc = static_cast<inc_t>(1);
+        auto M = static_cast<inc_t>(sz);
+        auto N = static_cast<inc_t>(sz);
+        auto alpha = TestType(1);
+        auto rsa = static_cast<inc_t>(rres.strides()[0]);
+        auto csa = static_cast<inc_t>(rres.strides()[1]);
+        
+        blis_ger(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, M, N, &alpha, a.data(), inc, a.data(), inc, rres.data(), rsa, csa);
+        amt::outer_prod(lres, a, a, std::nullopt);
+
+        for(auto i = 0ul; i < sz; ++i){
+            for(auto j = 0ul; j < sz; ++j)
+                REQUIRE(Approx(lres(i,j)) == rres(i,j));
+        }
+    }
+    
+}
+
+TEMPLATE_TEST_CASE( "First Order Rectangular Vector Vector Outer Product for Range[Start: 2, End: 32, Step: 1]", "[first_order_outer_prod]", float, double ) {
     namespace ub = boost::numeric::ublas;
 
     constexpr std::size_t max_size = 1 << 5;
@@ -96,8 +133,53 @@ TEMPLATE_TEST_CASE( "Rectangular Vector Vector Outer Product for Range[Start: 2,
             // amt::blas::outer_prod<TestType>(amt::blas::ColMajor, M, N, TestType{1}, a.data(), inc, b.data(), inc, rres.data(), M);
             amt::outer_prod(lres, a, b, std::nullopt);
 
-            for(auto i = 0ul; i < (m * n); ++i)
-                REQUIRE(Approx(lres[i]) == rres[i]);
+            for(auto i = 0ul; i < m; ++i){
+                for(auto j = 0ul; j < n; ++j)
+                    REQUIRE(Approx(lres(i,j)) == rres(i,j));
+            }
+        }
+    }
+
+}
+
+TEMPLATE_TEST_CASE( "Last Order Rectangular Vector Vector Outer Product for Range[Start: 2, End: 32, Step: 1]", "[last_order_outer_prod]", float, double ) {
+    namespace ub = boost::numeric::ublas;
+
+    constexpr std::size_t max_size = 1 << 5;
+    std::vector<std::size_t> sizes(max_size);
+    std::iota(sizes.begin(), sizes.end(), 2u);
+    
+    REQUIRE(sizes.size() == max_size);
+
+
+    for(auto const& m : sizes){
+        VectorType<TestType> a(ub::extents<2>{1,m});
+        std::iota(a.begin(), a.end(), 1);
+        for(auto const& n : sizes){
+            VectorType<TestType> b(ub::extents<2>{1,n});
+            std::iota(b.begin(), b.end(), 1);
+            
+            ub::dynamic_tensor<TestType, ub::layout::last_order> lres(ub::extents<>{m, n});
+            ub::dynamic_tensor<TestType, ub::layout::last_order> rres(ub::extents<>{m, n});
+            
+            auto inc = static_cast<inc_t>(1);
+            auto M = static_cast<inc_t>(m);
+            auto N = static_cast<inc_t>(n);
+            auto alpha = TestType(1);
+            auto aptr = a.data();
+            auto bptr = b.data();
+            auto cptr = rres.data();
+            auto rsa = static_cast<inc_t>(rres.strides()[0]);
+            auto csa = static_cast<inc_t>(rres.strides()[1]);
+            
+            blis_ger(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, M, N, &alpha, aptr, inc, bptr, inc, cptr, rsa, csa);
+            // amt::blas::outer_prod<TestType>(amt::blas::ColMajor, M, N, TestType{1}, a.data(), inc, b.data(), inc, rres.data(), M);
+            amt::outer_prod(lres, a, b, std::nullopt);
+
+            for(auto i = 0ul; i < m; ++i){
+                for(auto j = 0ul; j < n; ++j)
+                    REQUIRE(Approx(lres(i,j)) == rres(i,j));
+            }
         }
     }
 
