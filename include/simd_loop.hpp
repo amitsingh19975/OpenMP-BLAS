86842a6f4cbf8b5c85d0dc7e598305cd87be9bc3
Amit Singh <amitsingh19975@gmail.com>
2021-04-01 22:37:28 +0530

    Improve the column-major mtv performance
    Now we store the first element of the input vector in the
    registor.

--- include/simd_loop.hpp
+++ include/simd_loop.hpp
@@ -58,29 +58,28 @@ namespace amt::impl{
         template<typename ValueType, typename SizeType>
         AMT_ALWAYS_INLINE void operator()(ValueType* c, ValueType const* a, ValueType const* b, SizeType const n, SizeType const w) const noexcept{
             #define AMT_UNROLL_STATEMENT(OFFSET,C,A,B,W,I) C[I] += A[I + W * OFFSET] * b[OFFSET];
-            
+            auto const cst = b[0];
             #pragma omp simd
             for(auto i = 0ul; i < n; ++i){
-                if constexpr(N == 1){
-                    AMT_UNROLL_LOOP1(c,a,b,w,i);
-                }else if constexpr(N == 2){
-                    AMT_UNROLL_LOOP2(c,a,b,w,i);
+                c[i] += a[i] * cst;
+                if constexpr(N == 2){
+                    AMT_UNROLL_LOOP1_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 4){
-                    AMT_UNROLL_LOOP4(c,a,b,w,i);
+                    AMT_UNROLL_LOOP3_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 8){
-                    AMT_UNROLL_LOOP8(c,a,b,w,i);
+                    AMT_UNROLL_LOOP7_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 16){
-                    AMT_UNROLL_LOOP16(c,a,b,w,i);
+                    AMT_UNROLL_LOOP15_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 32){
-                    AMT_UNROLL_LOOP32(c,a,b,w,i);
+                    AMT_UNROLL_LOOP31_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 64){
-                    AMT_UNROLL_LOOP64(c,a,b,w,i);
+                    AMT_UNROLL_LOOP63_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 128){
-                    AMT_UNROLL_LOOP128(c,a,b,w,i);
+                    AMT_UNROLL_LOOP127_IMPL(1,c,a,b,w,i);
                 }else if constexpr(N == 256){
-                    AMT_UNROLL_LOOP256(c,a,b,w,i);
-                }else{
-                    AMT_UNROLL_LOOP512(c,a,b,w,i);
+                    AMT_UNROLL_LOOP255_IMPL(1,c,a,b,w,i);
+                }else if constexpr(N == 512){
+                    AMT_UNROLL_LOOP511_IMPL(1,c,a,b,w,i);
                 }
             }
             
@@ -119,13 +118,21 @@ namespace amt::impl{
 #undef AMT_UNROLL_LOOP512
 #undef AMT_UNROLL_LOOP1_IMPL
 #undef AMT_UNROLL_LOOP2_IMPL
+#undef AMT_UNROLL_LOOP3_IMPL
 #undef AMT_UNROLL_LOOP4_IMPL
+#undef AMT_UNROLL_LOOP7_IMPL
 #undef AMT_UNROLL_LOOP8_IMPL
+#undef AMT_UNROLL_LOOP15_IMPL
 #undef AMT_UNROLL_LOOP16_IMPL
+#undef AMT_UNROLL_LOOP31_IMPL
 #undef AMT_UNROLL_LOOP32_IMPL
+#undef AMT_UNROLL_LOOP63_IMPL
 #undef AMT_UNROLL_LOOP64_IMPL
+#undef AMT_UNROLL_LOOP127_IMPL
 #undef AMT_UNROLL_LOOP128_IMPL
+#undef AMT_UNROLL_LOOP255_IMPL
 #undef AMT_UNROLL_LOOP256_IMPL
+#undef AMT_UNROLL_LOOP511_IMPL
 #undef AMT_UNROLL_LOOP512_IMPL
 
 #endif // AMT_BENCHMARK_SIMD_LOOP_HPP
