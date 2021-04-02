c5ce0508c3b92728946bf7c4909593cbb013014c
Amit Singh <amitsingh19975@gmail.com>
2021-04-02 14:32:32 +0530

    remove unroll macros, instead use GNU unroll pragma

--- include/simd_loop.hpp
+++ include/simd_loop.hpp
@@ -51,39 +51,23 @@ namespace amt::impl{
         constexpr static auto type = SIMD_PROD_TYPE::MTV;
         constexpr static std::size_t size = N;
         static_assert(
-            (N < 1024ul) && N && !(N & (N - 1)),
-            "N should be a power of 2 and less than 1024, but not 0"
+            N && !(N & (N - 1)),
+            "N should be a power of 2, except 0"
         );
 
         template<typename ValueType, typename SizeType>
         AMT_ALWAYS_INLINE void operator()(ValueType* c, ValueType const* a, ValueType const* b, SizeType const n, SizeType const w) const noexcept{
-            #define AMT_UNROLL_STATEMENT(OFFSET,C,A,B,W,I) C[I] += A[I + W * OFFSET] * b[OFFSET];
             auto const cst = b[0];
             #pragma omp simd
             for(auto i = 0ul; i < n; ++i){
                 c[i] += a[i] * cst;
-                if constexpr(N == 2){
-                    AMT_UNROLL_LOOP1_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 4){
-                    AMT_UNROLL_LOOP3_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 8){
-                    AMT_UNROLL_LOOP7_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 16){
-                    AMT_UNROLL_LOOP15_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 32){
-                    AMT_UNROLL_LOOP31_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 64){
-                    AMT_UNROLL_LOOP63_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 128){
-                    AMT_UNROLL_LOOP127_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 256){
-                    AMT_UNROLL_LOOP255_IMPL(1,c,a,b,w,i);
-                }else if constexpr(N == 512){
-                    AMT_UNROLL_LOOP511_IMPL(1,c,a,b,w,i);
+                if constexpr(N > 1){
+                    #pragma unroll(N-1)
+                    for(auto j = 1ul; j < N; ++j){
+                        c[i] += a[i + w * j] * b[j];
+                    }
                 }
             }
-            
-            #undef AMT_UNROLL_STATEMENT
         }
 
     };
@@ -104,35 +88,5 @@ namespace amt::impl{
     };
     
 } // namespace amt::impl
-
-
-#undef AMT_UNROLL_LOOP1
-#undef AMT_UNROLL_LOOP2
-#undef AMT_UNROLL_LOOP4
-#undef AMT_UNROLL_LOOP8
-#undef AMT_UNROLL_LOOP16
-#undef AMT_UNROLL_LOOP32
-#undef AMT_UNROLL_LOOP64
-#undef AMT_UNROLL_LOOP128
-#undef AMT_UNROLL_LOOP256
-#undef AMT_UNROLL_LOOP512
-#undef AMT_UNROLL_LOOP1_IMPL
-#undef AMT_UNROLL_LOOP2_IMPL
-#undef AMT_UNROLL_LOOP3_IMPL
-#undef AMT_UNROLL_LOOP4_IMPL
-#undef AMT_UNROLL_LOOP7_IMPL
-#undef AMT_UNROLL_LOOP8_IMPL
-#undef AMT_UNROLL_LOOP15_IMPL
-#undef AMT_UNROLL_LOOP16_IMPL
-#undef AMT_UNROLL_LOOP31_IMPL
-#undef AMT_UNROLL_LOOP32_IMPL
-#undef AMT_UNROLL_LOOP63_IMPL
-#undef AMT_UNROLL_LOOP64_IMPL
-#undef AMT_UNROLL_LOOP127_IMPL
-#undef AMT_UNROLL_LOOP128_IMPL
-#undef AMT_UNROLL_LOOP255_IMPL
-#undef AMT_UNROLL_LOOP256_IMPL
-#undef AMT_UNROLL_LOOP511_IMPL
-#undef AMT_UNROLL_LOOP512_IMPL
 
 #endif // AMT_BENCHMARK_SIMD_LOOP_HPP
