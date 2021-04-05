e2929d8f0f9c0f256d5e57f3f2b632b0427a24c5
Amit Singh <amitsingh19975@gmail.com>
2021-04-05 20:19:46 +0530

    enable the tests

--- test/test_list.cmake
+++ test/test_list.cmake
@@ -1,7 +1,7 @@
 set(TEST_LIST
     "test.mtm.cpp"
-    # "test.mtv.cpp"
-    # "test.vtm.cpp"
-    # "test.dot_prod.cpp"
-    # "test.outer_prod.cpp"
+    "test.mtv.cpp"
+    "test.vtm.cpp"
+    "test.dot_prod.cpp"
+    "test.outer_prod.cpp"
 )
